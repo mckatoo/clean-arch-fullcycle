5f60e4d8658e36e23fe7e9cf07a2f480bac2701e
Milton Carlos Katoo <mckatoo@gmail.com>
2022-06-17 14:48:25 -0300

    Init project

---
+++ src/about-page.entity.ts
@@ -0,0 +1,25 @@
+import { BaseEntity } from "./base.entity"
+
+export class AboutPage extends BaseEntity {
+  skills?: string[] = []
+  title: string = ''
+  description: string = ''
+  image?: {
+    src: string
+    alt: string
+  }
+
+  constructor (aboutPage?: AboutPage) {
+    super({})
+
+    if (aboutPage) {
+      const { id, skills, title, description, image } = aboutPage
+
+      if (id !== undefined) this.id = id
+      this.skills = skills || []
+      this.title = title
+      this.description = description
+      this.image = image
+    }
+  }
+}
