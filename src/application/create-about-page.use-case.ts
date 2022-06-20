3e9a432625ccdfa5b69a9f55ef4bf943cc7a4d02
Milton Carlos Katoo <mckatoo@gmail.com>
2022-06-20 19:50:48 -0300

    ♻️ (refactor): Organize files

---
+++ src/application/create-about-page.use-case.ts
@@ -0,0 +1,27 @@
+import { SkillProps, ImageProps, AboutPage } from "../domain/about-page.entity";
+import { AboutPageRepositoryInterface } from "../domain/about-page.repository";
+
+type CreateAboutPageInput = {
+  title: string;
+  description: string;
+  skills?: SkillProps[];
+  image?: ImageProps;
+};
+
+type CreateAboutPageOutput = {
+  title: string;
+  description: string;
+  skills: SkillProps[];
+  image: ImageProps;
+};
+
+export class CreateAboutPageUseCase {
+  constructor(private aboutPageRepository: AboutPageRepositoryInterface) {}
+
+  async execute(input: CreateAboutPageInput): Promise<CreateAboutPageOutput> {
+    const aboutPage = new AboutPage(input);
+    await this.aboutPageRepository.insert(aboutPage);
+
+    return aboutPage.toJson();
+  }
+}
---
+++ src/infra/db/about-page-memory.repository.test.ts
@@ -0,0 +1,18 @@
+import { AboutPage, AboutPageProps } from "../../domain/about-page.entity";
+import { AboutPageMemoryRepository } from "./about-page-memory.repository";
+
+describe("AboutPageMemory Repository", () => {
+  it("should insert a new about page", async () => {
+    const repository = new AboutPageMemoryRepository();
+    const aboutPageData: AboutPageProps = {
+      title: "title",
+      description: "description",
+    };
+    const aboutPage = new AboutPage(aboutPageData);
+    expect(repository.items).toHaveLength(0);
+
+    await repository.insert(aboutPage);
+    expect(repository.items).toHaveLength(1);
+    expect(repository.items).toStrictEqual([aboutPage]);
+  });
+});
