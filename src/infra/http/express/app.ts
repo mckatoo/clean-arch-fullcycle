ac0ce9c84903e4ccf483eaee5370f576bca5af7c
Milton Carlos Katoo <mckatoo@gmail.com>
2022-06-25 20:40:54 -0300

    menu options

--- src/infra/http/express/app.ts
+++ src/infra/http/express/app.ts
@@ -15,27 +15,34 @@ app.post("/about", async (req: Request, res: Response) => {
   res.status(201).json(output);
 });
 
+app.post("/menu", (req: Request, res: Response) => { 
+  const {menu} = req.body
+  const publicLinks = [
+    { label: "Sobre", to: "about" },
+    { label: "Habilidades", to: "skills" },
+    { label: "Projetos", to: "projects" },
+    { label: "Contatos", to: "contact" },
+  ]
+
+  const adminLinks = [
+    { label: "Dashboard", to: "admin" },
+  ]
+
+  const social = {
+    linkedin: "mckatoo",
+    github: "mckatoo",
+    youtube: "UCc1e1mclC9o5OnQU87PcU1g",
+  }
+
+  const links = (menu === 'public') ? publicLinks : adminLinks
+  
+  res.json({social, links})
+})
+
 // Status
 app.get("/", (_req: Request, res: Response) => {
   res.status(200).send("OK");
 });
-
-app.post("/menu", (req: Request, res: Response) => { 
-const links = [
-  { label: "Sobre", to: "about" },
-  { label: "Habilidades", to: "skills" },
-  { label: "Projetos", to: "projects" },
-  { label: "Contatos", to: "contact" }
-]
-
-const social = {
-  linkedin: "mckatoo",
-  github: "mckatoo",
-  youtube: "UCc1e1mclC9o5OnQU87PcU1g",
-}
-  
-  res.json({ links, social })
-})
 
 // Error Handling
 app.use((error: any, req: Request, res: Response, _: NextFunction) => {
