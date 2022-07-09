2d007a6255e71b8ca8cecfdaee74726a40128560
Milton Carlos Katoo <mckatoo@gmail.com>
2022-07-09 12:00:26 -0300

    Update props with tests

--- src/domain/menu/menu.entity.ts
+++ src/domain/menu/menu.entity.ts
@@ -1,12 +1,16 @@
+import { randomUUID } from "crypto";
+
 export type MenuProps = {
   label: string
   to: string
 };
 
 export class Menu {
+  readonly id: string;
   props: Required<MenuProps>;
 
-  constructor(props: MenuProps) {
+  constructor(props: MenuProps, id?: string) {
+    this.id = id || randomUUID()
     this.props = {
       ...props,
       label: props.label || "",
