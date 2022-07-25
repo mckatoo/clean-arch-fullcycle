c04af4c423ea4857200a46c5c7bf6dcd5226b97b
Milton Carlos Katoo <mckatoo@gmail.com>
2022-07-25 20:26:21 -0300

    :recycle: refactor
    Structured data from menu route

--- src/infra/http/express/routes/menu/menu.test.ts
+++ src/infra/http/express/routes/menu/menu.test.ts
@@ -4,14 +4,23 @@ import app from "../../app";
 describe('Menu', () => {
   it('should get menu', async () => {
     const publicLinks = [
-      { label: "Sobre", to: "app/about" },
-      { label: "Habilidades", to: "app/skills" },
-      { label: "Projetos", to: "app/projects" },
-      { label: "Contatos", to: "app/contact" },
+      { menu_item_id: 1, menu_id: 1, label: "Sobre", to: "app/about" },
+      { menu_item_id: 2, menu_id: 1, label: "Habilidades", to: "app/skills" },
+      { menu_item_id: 3, menu_id: 1, label: "Projetos", to: "app/projects" },
+      { menu_item_id: 4, menu_id: 1, label: "Contatos", to: "app/contact" }
     ]
+    const socialLinks = {
+      social_id: 1,
+      linkedin: "mckatoo-linkedin",
+      github: "mckatoo-github",
+      youtube: "mckatoo-youtube",
+      user_id: 1
+    }
+
     const response = await request(app).get("/menu/public").send()
 
     expect(response.body).toHaveProperty('links', publicLinks)
+    expect(response.body).toHaveProperty('social', socialLinks)
   })
 
   it('should create menu', async () => {
