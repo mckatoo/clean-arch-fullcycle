ea07a6d6c5be673a51b729e9cd5146ac20713b9e
Milton Carlos Katoo <mckatoo@gmail.com>
2022-08-16 10:45:19 -0300

    Mocked data from menu

--- src/infra/http/express/routes/menu/menu.test.ts
+++ src/infra/http/express/routes/menu/menu.test.ts
@@ -1,37 +1,21 @@
 import request from "supertest";
+
 import app from "../../app";
+import { mockedPublicMenu } from "./mock";
 
 describe('Menu', () => {
-  it('should get menu', async () => {
-    const publicLinks = [
-      { menu_item_id: 1, menu_id: 1, label: "Sobre", to: "app/about" },
-      { menu_item_id: 2, menu_id: 1, label: "Habilidades", to: "app/skills" },
-      { menu_item_id: 3, menu_id: 1, label: "Projetos", to: "app/projects" },
-      { menu_item_id: 4, menu_id: 1, label: "Contatos", to: "app/contact" }
-    ]
-    const socialLinks = {
-      social_id: 1,
-      linkedin: "mckatoo-linkedin",
-      github: "mckatoo-github",
-      youtube: "mckatoo-youtube",
-      user_id: 1
-    }
-
-    const response = await request(app).get("/menu/public").send()
-
-    expect(response.body).toHaveProperty('links', publicLinks)
-    expect(response.body).toHaveProperty('social', socialLinks)
-  })
-
   it('should create menu', async () => {
-    const adminLinks = [
-      { label: "Dashboard", to: "admin" },
-    ]
     const response = await request(app).post("/menu").send({
-      menu: adminLinks
+      menu: mockedPublicMenu
     })
 
     expect(response.status).toBe(201)
   });
+
+  it('should get menu', async () => {
+    const response = await request(app).get("/menu/public").send()
+
+    expect(response.body).toMatchObject(mockedPublicMenu)
+  })
 
 })
